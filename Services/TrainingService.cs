ceb3177ecda939cef2d1194c79768551e6516fd3
KatarzynaStrumidlo <kasia.dorota.20.11@gmail.com>
2023-04-24 21:41:11 +0200

    Problem with app.

---
+++ Services/TrainingService.cs
@@ -0,0 +1,17 @@
+﻿using TrainingManager.Domains.Tables;
+using TrainingManager.Models;
+
+namespace TrainingManager.RestService.Services
+{
+    public interface ITrainingService
+    {
+        List<Training> GetTrainings();
+    }
+    public class TrainingService : ITrainingService
+    {
+        public List<Training> GetTrainings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
