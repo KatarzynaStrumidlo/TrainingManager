e4cc36a1da0238c81f95a12a0dfe550a05af4c47
KatarzynaStrumidlo <kstrumid@createctrl.com>
2023-05-11 08:35:23 +0200

    Fix problem with references. Prepare files to implement crud actions.

--- Controllers/TrainingManagerController.cs
+++ Controllers/TrainingManagerController.cs
@@ -1,29 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using TrainingManager.RestService.Services;
 
-namespace TrainingManager.RestService.Controllers
+namespace TrainingManager.RestService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TrainingManagerController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class TrainingManagerController : ControllerBase
+    private readonly ITrainingService _trainingService;
+
+    public TrainingManagerController(ITrainingService trainingService)
     {
-        private readonly ITrainingService _trainingService;
-
-        public TrainingManagerController(ITrainingService trainingService)
-        {
-            _trainingService = trainingService;
-        }
-
-        //[HttpGet(Name = "GetWeatherForecast")]
-        //public IEnumerable<WeatherForecast> Get()
-        //{
-        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        Date = DateTime.Now.AddDays(index),
-        //        TemperatureC = Random.Shared.Next(-20, 55),
-        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        //    })
-        //    .ToArray();
-        //}
+        _trainingService = trainingService;
     }
+
+    //[HttpGet(Name = "GetWeatherForecast")]
+    //public IEnumerable<WeatherForecast> Get()
+    //{
+    //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    //    {
+    //        Date = DateTime.Now.AddDays(index),
+    //        TemperatureC = Random.Shared.Next(-20, 55),
+    //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    //    })
+    //    .ToArray();
+    //}
 }
--- Services/TrainingService.cs
+++ Services/TrainingService.cs
@@ -1,17 +1,15 @@
-﻿using TrainingManager.Domains.Tables;
-using TrainingManager.Models;
+﻿using TrainingManager.Models;
 
-namespace TrainingManager.RestService.Services
+namespace TrainingManager.RestService.Services;
+
+public interface ITrainingService
 {
-    public interface ITrainingService
+    List<Training> GetTrainings();
+}
+public class TrainingService : ITrainingService
+{
+    public List<Training> GetTrainings()
     {
-        List<Training> GetTrainings();
-    }
-    public class TrainingService : ITrainingService
-    {
-        public List<Training> GetTrainings()
-        {
-            throw new NotImplementedException();
-        }
+        throw new NotImplementedException();
     }
 }
